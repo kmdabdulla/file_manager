091f5f38e55ebdb87fc198360e534f53f6c75111
kmdabdulla <abdulla@archsystem.localdomain>
2021-02-15 00:32:39 -0500

    remove icon search bar

--- resources/views/filemanager.blade.php
+++ resources/views/filemanager.blade.php
@@ -47,16 +47,6 @@
         <div class="card">
           <div class="card-header">
             <h3 class="card-title">Uploaded Files</h3>
-            <div class="card-tools">
-              <div class="input-group input-group-sm" style="width: 150px;">
-                <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-                <div class="input-group-append">
-                  <button type="submit" class="btn btn-default">
-                    <i class="fas fa-search"></i>
-                  </button>
-                </div>
-              </div>
-            </div>
           </div>
           <div class="card-body table-responsive p-0" style="height: 200px;">
             <table class="table table-head-fixed text-nowrap">
